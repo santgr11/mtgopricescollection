79bc32d2881f4cc05f281275cef60e7d10a54a0a
Santiago Grangetto <santiagograngetto@gmail.com>
2018-06-14 15:02:30 -0300

    Add files via upload
    # Fixed the "no spider found"
    # Fixed text for cards with a ' and double cards

--- mtgo/mtgo/spiders/pricebot.py
+++ mtgo/mtgo/spiders/pricebot.py
@@ -1,46 +1,51 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import os
+from mtgo.items import MtgoItem
 
 #Defining a list of the cards to use in multiple places.
 cardslist = []
 
 class PricebotSpider(scrapy.Spider):
-    name = 'pricebot'
+    name = "pricebot"
     allowed_domains = ['www.goatbots.com']
     start_urls = []
     
-    cards = open("collection.txt")
+    
+    path = os.path.join("collections", "collection2.txt")
+    cards = open(path)
     each = cards.readlines()
    
     #Adding the cards to the list from the txt file
     for line in each[4:-1]:
-        name = line[60:-4]
-        if name not in cardslist:
-            cardslist.append(name)
+        card = line[60:-4]
+        card = card.replace(",", "").lower()
+        card = card.replace(" ", "-")
+        card = card.replace("\\", "")
+        card = card.replace('"', "")
+        card = card.replace("\'", "")        
+        if card[-2:] == "-/":
+            card = card[:-2]
+        card = card.replace("/", "-")
+        if card not in cardslist and card not in ("forest", "plain", "island", "swamp", "mountain"):            
+            cardslist.append(card)
         
     base = "https://www.goatbots.com/card/"
     
     #Here we create a link for every card to append to the start_urls
-    for card in cardslist:
-        fcard = card.replace(",", "")
-        fcard = fcard.replace(" ", "-")
-        fcard = fcard.replace("\\", "")
-        fcard = fcard.replace('"', "")
-        start_urls.append(base + fcard)
-        
-        
-        
+    for card in cardslist:        
+        start_urls.append(base + card)
+     
+            
     def parse(self, response):
         #extracting content
+        item = MtgoItem()
         
-        price = response.css(".price_value::text").extract_first()
+        item["name"] = response.url[30:]
+                           
+        try:
+            item["price"] = int(response.css(".price_value::text").extract_first())
+        except ValueError:
+            item["price"] = response.css(".price_value::text").extract_first()
         
-        for item in zip(price):
-            a = 0
-            scraped_data = {
-                'card' : cardslist[a],
-                'price' : item[0]
-            }
-            a += 1
-        
-        yield scraped_data
+        yield item 
